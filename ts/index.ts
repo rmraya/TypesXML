b6876cc1663807fd479aa022b8a829e0c8b8fe32
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-01-24 10:39:59 -0300

    prepared for npm package distribution

---
+++ ts/index.ts
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse   License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
+export { Attribute } from './Attribute';
+export { CData } from './CData';
+export { Comment } from './Comment';
+export { Document } from './Document';
+export { Element } from './Element';
+export { ProcessingInstruction } from './ProcessingInstruction';
+export { TextNode } from './TextNode';
+export { XMLNode } from './XMLNode';
+export { XMLParser } from './XMLParser';
+export { XMLUtils } from './XMLUtils';
+export { XMLWriter } from './XMLWriter';
