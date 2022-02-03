60d4073ff6774b4ff11e8fec44e7b0659baffd8d
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-02-03 20:45:55 -0300

    Updated code

--- ts/XMLUtils.ts
+++ ts/XMLUtils.ts
@@ -21,8 +21,31 @@ export class XMLUtils {
         return text.replace('"', '&quot;');
     }
 
-    static normalizeLines(text: string) {
+    static normalizeLines(text: string): string {
         let result: string = text.replace('\r\n', '\n');
         return result.replace('\r', '\n');
     }
+
+    static validXmlChars(text: string): string {
+        // From xml spec valid chars: 
+        // #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]     
+        // any Unicode character, excluding the surrogate blocks, FFFE, and FFFF. 
+        let result: string = '';
+        let length: number = text.length;
+        for (let i = 0; i < length; i++) {
+            let c: number = text.charCodeAt(i);
+            if (XMLUtils.isValidXmlChar(c)) {
+                result += String.fromCharCode(c);
+            }
+        }
+        return result;
+    }
+
+    static isValidXmlChar(c: number): boolean {
+        return c === 0x9 || c == 0xA || c === 0xD ||
+            (c >= 0x20 && c <= 0xD7FF) ||
+            (c >= 0xE000 && c <= 0xFFFD) ||
+            (c >= 0x10000 && c <= 0x10FFFF);
+    }
+
 }
