20e130d1c62c8727cc8a9569ed0cdb2cc29a57b2
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-04-14 09:47:37 -0300

    Initial code for grammars support

---
+++ ts/grammar/Grammar.ts
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse   License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
+import { ContentModel } from "./ContentModel";
+
+export class Grammar {
+
+    private models: Map<string, ContentModel>;
+
+    constructor() {
+        this.models = new Map();
+    }
+
+    getContentModel(elementName: string): ContentModel {
+        return this.models.get(elementName);
+    }
+}
