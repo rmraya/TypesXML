ba025eec2d3c62c1a613eda6621c7600a8ecc769
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2024-03-02 14:01:10 -0300

    Updated index & copyright

--- ts/XMLAttribute.ts
+++ ts/XMLAttribute.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Maxprograms.
+ * Copyright (c) 2023 - 2024 Maxprograms.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse   License 1.0
