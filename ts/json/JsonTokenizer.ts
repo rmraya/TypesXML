2e6a4f18cdf370cd3b4f0b85d0fb060661b87967
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-01-01 12:38:43 -0300

    Adjusted copyright year

--- ts/json/JsonTokenizer.ts
+++ ts/json/JsonTokenizer.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Maxprograms.
+ * Copyright (c) 2023-2026 Maxprograms.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse   License 1.0
--- ts/tests/JsonConversionTest.ts
+++ ts/tests/JsonConversionTest.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2025 Maxprograms.
+ * Copyright (c) 2023-2026 Maxprograms.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse   License 1.0
@@ -21,15 +21,12 @@ import {
     JsonElementObject,
     JsonValue,
     XmlJsonDocument,
-    jsonFileToXmlDocument,
     jsonFileToXmlFile,
     jsonObjectToXmlDocument,
     jsonObjectToXmlFile,
     jsonStreamToXmlDocument,
     xmlDocumentToJsonObject,
-    xmlDocumentToJsonFile,
     xmlFileToJsonFile,
-    xmlFileToJsonObject,
     xmlStreamToJsonObject,
     xmlStringToJsonFile,
     xmlStringToJsonObject
