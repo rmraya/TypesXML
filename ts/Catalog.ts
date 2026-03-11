f35d62032c36946ffb9ec2b18df85482755ce2d8
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-03-11 11:55:55 -0300

    Updated catalog support

--- ts/Catalog.ts
+++ ts/Catalog.ts
@@ -25,28 +25,47 @@ export class Catalog {
     publicCatalog: Map<string, string>;
     uriCatalog: Map<string, string>;
     dtdCatalog: Map<string, string>;
+    systemSuffixCatalog: Map<string, string>;
+    uriSuffixCatalog: Map<string, string>;
 
     uriRewrites: Array<string[]>;
     systemRewrites: Array<string[]>;
+    delegatePublicEntries: Array<string[]>;
+    delegateSystemEntries: Array<string[]>;
+    delegateURIEntries: Array<string[]>;
+
     workDir: string;
     base: string;
+    prefer: string;
+    visitedCatalogs: Set<string>;
 
-    constructor(catalogFile: string) {
+    constructor(catalogFile: string, visitedCatalogs?: Set<string>) {
         if (!isAbsolute(catalogFile)) {
             throw new Error('Catalog file must be absolute: ' + catalogFile);
         }
         if (!existsSync(catalogFile)) {
             throw new Error('Catalog file ' + catalogFile + ' not found');
         }
+        this.visitedCatalogs = visitedCatalogs ?? new Set<string>();
+        if (this.visitedCatalogs.has(catalogFile)) {
+            throw new Error('Circular catalog reference detected: ' + catalogFile);
+        }
+        this.visitedCatalogs.add(catalogFile);
 
         this.systemCatalog = new Map<string, string>();
         this.publicCatalog = new Map<string, string>();
         this.uriCatalog = new Map<string, string>();
         this.dtdCatalog = new Map<string, string>();
+        this.systemSuffixCatalog = new Map<string, string>();
+        this.uriSuffixCatalog = new Map<string, string>();
         this.uriRewrites = new Array<string[]>();
         this.systemRewrites = new Array<string[]>();
+        this.delegatePublicEntries = new Array<string[]>();
+        this.delegateSystemEntries = new Array<string[]>();
+        this.delegateURIEntries = new Array<string[]>();
         this.workDir = dirname(catalogFile);
         this.base = '';
+        this.prefer = 'public';
 
         let contentHandler: DOMBuilder = new DOMBuilder();
         let parser: SAXParser = new SAXParser();
@@ -62,6 +81,10 @@ export class Catalog {
         }
         if (catalogRoot.getName() !== 'catalog') {
             throw new Error('Catalog root element must be <catalog>');
+        }
+        let preferAttr: XMLAttribute | undefined = catalogRoot.getAttribute('prefer');
+        if (preferAttr) {
+            this.prefer = preferAttr.getValue();
         }
         this.recurse(catalogRoot);
     }
@@ -159,7 +182,7 @@ export class Catalog {
                     throw new Error('uriStartString attribute is required for <rewriteURI>');
                 }
                 let pair: string[] = [uriStartString.getValue(), uri];
-                if (!this.uriRewrites.includes(pair)) {
+                if (!this.uriRewrites.some(p => p[0] === pair[0])) {
                     this.uriRewrites.push(pair);
                 }
             }
@@ -174,8 +197,81 @@ export class Catalog {
                     throw new Error('systemIdStartString attribute is required for <rewriteSystem>');
                 }
                 let pair: string[] = [systemIdStartString.getValue(), uri];
-                if (!this.systemRewrites.includes(pair)) {
+                if (!this.systemRewrites.some(p => p[0] === pair[0])) {
                     this.systemRewrites.push(pair);
+                }
+            }
+            if (child.getName() === 'systemSuffix') {
+                let systemIdSuffix: XMLAttribute | undefined = child.getAttribute("systemIdSuffix");
+                if (!systemIdSuffix) {
+                    throw new Error('systemIdSuffix attribute is required for <systemSuffix>');
+                }
+                let uriAttribute: XMLAttribute | undefined = child.getAttribute("uri");
+                if (!uriAttribute) {
+                    throw new Error('uri attribute is required for <systemSuffix>');
+                }
+                let uri: string = this.makeAbsolute(uriAttribute.getValue());
+                if (existsSync(uri) && !this.systemSuffixCatalog.has(systemIdSuffix.getValue())) {
+                    this.systemSuffixCatalog.set(systemIdSuffix.getValue(), uri);
+                }
+            }
+            if (child.getName() === 'uriSuffix') {
+                let uriSuffix: XMLAttribute | undefined = child.getAttribute("uriSuffix");
+                if (!uriSuffix) {
+                    throw new Error('uriSuffix attribute is required for <uriSuffix>');
+                }
+                let uriAttribute: XMLAttribute | undefined = child.getAttribute("uri");
+                if (!uriAttribute) {
+                    throw new Error('uri attribute is required for <uriSuffix>');
+                }
+                let uri: string = this.makeAbsolute(uriAttribute.getValue());
+                if (existsSync(uri) && !this.uriSuffixCatalog.has(uriSuffix.getValue())) {
+                    this.uriSuffixCatalog.set(uriSuffix.getValue(), uri);
+                }
+            }
+            if (child.getName() === 'delegatePublic') {
+                let publicIdStartString: XMLAttribute | undefined = child.getAttribute("publicIdStartString");
+                if (!publicIdStartString) {
+                    throw new Error('publicIdStartString attribute is required for <delegatePublic>');
+                }
+                let catalogAttribute: XMLAttribute | undefined = child.getAttribute("catalog");
+                if (!catalogAttribute) {
+                    throw new Error('catalog attribute is required for <delegatePublic>');
+                }
+                let catalogPath: string = this.makeAbsolute(catalogAttribute.getValue());
+                let pair: string[] = [publicIdStartString.getValue(), catalogPath];
+                if (!this.delegatePublicEntries.some(p => p[0] === pair[0])) {
+                    this.delegatePublicEntries.push(pair);
+                }
+            }
+            if (child.getName() === 'delegateSystem') {
+                let systemIdStartString: XMLAttribute | undefined = child.getAttribute("systemIdStartString");
+                if (!systemIdStartString) {
+                    throw new Error('systemIdStartString attribute is required for <delegateSystem>');
+                }
+                let catalogAttribute: XMLAttribute | undefined = child.getAttribute("catalog");
+                if (!catalogAttribute) {
+                    throw new Error('catalog attribute is required for <delegateSystem>');
+                }
+                let catalogPath: string = this.makeAbsolute(catalogAttribute.getValue());
+                let pair: string[] = [systemIdStartString.getValue(), catalogPath];
+                if (!this.delegateSystemEntries.some(p => p[0] === pair[0])) {
+                    this.delegateSystemEntries.push(pair);
+                }
+            }
+            if (child.getName() === 'delegateURI') {
+                let uriStartString: XMLAttribute | undefined = child.getAttribute("uriStartString");
+                if (!uriStartString) {
+                    throw new Error('uriStartString attribute is required for <delegateURI>');
+                }
+                let catalogAttribute: XMLAttribute | undefined = child.getAttribute("catalog");
+                if (!catalogAttribute) {
+                    throw new Error('catalog attribute is required for <delegateURI>');
+                }
+                let catalogPath: string = this.makeAbsolute(catalogAttribute.getValue());
+                let pair: string[] = [uriStartString.getValue(), catalogPath];
+                if (!this.delegateURIEntries.some(p => p[0] === pair[0])) {
+                    this.delegateURIEntries.push(pair);
                 }
             }
             if (child.getName() === 'nextCatalog') {
@@ -183,8 +279,11 @@ export class Catalog {
                 if (!catalogAttribute) {
                     throw new Error('catalog attribute is required for <nextCatalog>');
                 }
-                let nextCatalog: string = this.makeAbsolute(catalogAttribute.getValue());
-                let catalog: Catalog = new Catalog(nextCatalog);
+                let nextCatalogPath: string = this.makeAbsolute(catalogAttribute.getValue());
+                if (this.visitedCatalogs.has(nextCatalogPath)) {
+                    throw new Error('Circular catalog reference detected: ' + nextCatalogPath);
+                }
+                let catalog: Catalog = new Catalog(nextCatalogPath, new Set(this.visitedCatalogs));
                 let map: Map<string, string> = catalog.getSystemCatalog();
                 map.forEach((value, key) => {
                     if (!this.systemCatalog.has(key)) {
@@ -209,16 +308,46 @@ export class Catalog {
                         this.dtdCatalog.set(key, value);
                     }
                 });
+                map = catalog.getSystemSuffixCatalog();
+                map.forEach((value, key) => {
+                    if (!this.systemSuffixCatalog.has(key)) {
+                        this.systemSuffixCatalog.set(key, value);
+                    }
+                });
+                map = catalog.getUriSuffixCatalog();
+                map.forEach((value, key) => {
+                    if (!this.uriSuffixCatalog.has(key)) {
+                        this.uriSuffixCatalog.set(key, value);
+                    }
+                });
                 let array: Array<string[]> = catalog.getUriRewrites();
                 array.forEach((value) => {
-                    if (!this.uriRewrites.includes(value)) {
+                    if (!this.uriRewrites.some(p => p[0] === value[0])) {
                         this.uriRewrites.push(value);
                     }
                 });
                 array = catalog.getSystemRewrites();
                 array.forEach((value) => {
-                    if (!this.systemRewrites.includes(value)) {
+                    if (!this.systemRewrites.some(p => p[0] === value[0])) {
                         this.systemRewrites.push(value);
+                    }
+                });
+                array = catalog.getDelegatePublicEntries();
+                array.forEach((value) => {
+                    if (!this.delegatePublicEntries.some(p => p[0] === value[0])) {
+                        this.delegatePublicEntries.push(value);
+                    }
+                });
+                array = catalog.getDelegateSystemEntries();
+                array.forEach((value) => {
+                    if (!this.delegateSystemEntries.some(p => p[0] === value[0])) {
+                        this.delegateSystemEntries.push(value);
+                    }
+                });
+                array = catalog.getDelegateURIEntries();
+                array.forEach((value) => {
+                    if (!this.delegateURIEntries.some(p => p[0] === value[0])) {
+                        this.delegateURIEntries.push(value);
                     }
                 });
             }
@@ -228,14 +357,13 @@ export class Catalog {
     }
 
     makeAbsolute(uri: string): string {
-        let file: string = this.base + uri;
-        if (!isAbsolute(file)) {
-            if (this.base !== '') {
-                return resolve(this.base, uri);
-            }
-            return resolve(this.workDir, uri);
+        if (isAbsolute(uri)) {
+            return uri;
         }
-        return this.base + uri;
+        if (this.base !== '') {
+            return resolve(this.base, uri);
+        }
+        return resolve(this.workDir, uri);
     }
 
     unwrapUrn(urn: string): string {
@@ -272,6 +400,14 @@ export class Catalog {
         return this.dtdCatalog;
     }
 
+    getSystemSuffixCatalog(): Map<string, string> {
+        return this.systemSuffixCatalog;
+    }
+
+    getUriSuffixCatalog(): Map<string, string> {
+        return this.uriSuffixCatalog;
+    }
+
     getUriRewrites(): Array<string[]> {
         return this.uriRewrites;
     }
@@ -280,7 +416,32 @@ export class Catalog {
         return this.systemRewrites;
     }
 
+    getDelegatePublicEntries(): Array<string[]> {
+        return this.delegatePublicEntries;
+    }
+
+    getDelegateSystemEntries(): Array<string[]> {
+        return this.delegateSystemEntries;
+    }
+
+    getDelegateURIEntries(): Array<string[]> {
+        return this.delegateURIEntries;
+    }
+
     resolveEntity(publicId: string, systemId: string): string | undefined {
+        if (this.prefer === 'system') {
+            if (systemId) {
+                let location: string | undefined = this.matchSystem(systemId);
+                if (location) {
+                    return location;
+                }
+            }
+            if (publicId) {
+                return this.matchPublic(publicId);
+            }
+            return undefined;
+        }
+        // default: prefer="public"
         if (publicId) {
             let location: string | undefined = this.matchPublic(publicId);
             if (location) {
@@ -292,13 +453,48 @@ export class Catalog {
 
     matchSystem(systemId: string): string | undefined {
         if (systemId) {
+            // Apply the rewrite with the longest matching prefix (spec: longest match wins)
+            let bestPrefix = '';
+            let bestRewriteUri = '';
             for (let pair of this.systemRewrites) {
-                if (systemId.startsWith(pair[0])) {
-                    systemId = pair[1] + systemId.substring(pair[0].length);
+                if (systemId.startsWith(pair[0]) && pair[0].length > bestPrefix.length) {
+                    bestPrefix = pair[0];
+                    bestRewriteUri = pair[1];
                 }
+            }
+            if (bestPrefix) {
+                systemId = bestRewriteUri + systemId.substring(bestPrefix.length);
+            }
+            // If any delegateSystem entry matches, search only those catalogs (spec: do not continue here)
+            let matchingDelegates = this.delegateSystemEntries
+                .filter(pair => systemId.startsWith(pair[0]))
+                .sort((a, b) => b[0].length - a[0].length);
+            if (matchingDelegates.length > 0) {
+                for (let pair of matchingDelegates) {
+                    if (existsSync(pair[1])) {
+                        let delegateCatalog = new Catalog(pair[1], new Set(this.visitedCatalogs));
+                        let result = delegateCatalog.matchSystem(systemId);
+                        if (result) {
+                            return result;
+                        }
+                    }
+                }
+                return undefined;
             }
             if (this.systemCatalog.has(systemId)) {
                 return this.systemCatalog.get(systemId);
+            }
+            // systemSuffix: longest matching suffix wins
+            let bestSuffix = '';
+            let bestSuffixUri: string | undefined;
+            for (let [suffix, uri] of this.systemSuffixCatalog) {
+                if (systemId.endsWith(suffix) && suffix.length > bestSuffix.length) {
+                    bestSuffix = suffix;
+                    bestSuffixUri = uri;
+                }
+            }
+            if (bestSuffixUri) {
+                return bestSuffixUri;
             }
             let fileName: string = basename(systemId);
             if (this.dtdCatalog.has(fileName)) {
@@ -312,6 +508,22 @@ export class Catalog {
         if (publicId.startsWith("urn:publicid:")) {
             publicId = this.unwrapUrn(publicId);
         }
+        // If any delegatePublic entry matches, search only those catalogs (spec: do not continue here)
+        let matchingDelegates = this.delegatePublicEntries
+            .filter(pair => publicId.startsWith(pair[0]))
+            .sort((a, b) => b[0].length - a[0].length);
+        if (matchingDelegates.length > 0) {
+            for (let pair of matchingDelegates) {
+                if (existsSync(pair[1])) {
+                    let delegateCatalog = new Catalog(pair[1], new Set(this.visitedCatalogs));
+                    let result = delegateCatalog.matchPublic(publicId);
+                    if (result) {
+                        return result;
+                    }
+                }
+            }
+            return undefined;
+        }
         if (this.publicCatalog.has(publicId)) {
             return this.publicCatalog.get(publicId);
         }
@@ -320,15 +532,48 @@ export class Catalog {
 
     matchURI(uri: string): string | undefined {
         if (uri) {
-            // Apply URI rewrites first
+            // Apply the rewrite with the longest matching prefix (spec: longest match wins)
+            let bestPrefix = '';
+            let bestRewriteUri = '';
             for (let pair of this.uriRewrites) {
-                if (uri.startsWith(pair[0])) {
-                    uri = pair[1] + uri.substring(pair[0].length);
+                if (uri.startsWith(pair[0]) && pair[0].length > bestPrefix.length) {
+                    bestPrefix = pair[0];
+                    bestRewriteUri = pair[1];
                 }
             }
-            // Look up in URI catalog
+            if (bestPrefix) {
+                uri = bestRewriteUri + uri.substring(bestPrefix.length);
+            }
+            // If any delegateURI entry matches, search only those catalogs (spec: do not continue here)
+            let matchingDelegates = this.delegateURIEntries
+                .filter(pair => uri.startsWith(pair[0]))
+                .sort((a, b) => b[0].length - a[0].length);
+            if (matchingDelegates.length > 0) {
+                for (let pair of matchingDelegates) {
+                    if (existsSync(pair[1])) {
+                        let delegateCatalog = new Catalog(pair[1], new Set(this.visitedCatalogs));
+                        let result = delegateCatalog.matchURI(uri);
+                        if (result) {
+                            return result;
+                        }
+                    }
+                }
+                return undefined;
+            }
             if (this.uriCatalog.has(uri)) {
                 return this.uriCatalog.get(uri);
+            }
+            // uriSuffix: longest matching suffix wins
+            let bestSuffix = '';
+            let bestSuffixUri: string | undefined;
+            for (let [suffix, suffixUri] of this.uriSuffixCatalog) {
+                if (uri.endsWith(suffix) && suffix.length > bestSuffix.length) {
+                    bestSuffix = suffix;
+                    bestSuffixUri = suffixUri;
+                }
+            }
+            if (bestSuffixUri) {
+                return bestSuffixUri;
             }
         }
         return undefined;
